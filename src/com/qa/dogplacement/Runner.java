33511dd2538269f5417528f6b1cfd1039656bb8d
Muvis84 <57877755+Muvis84@users.noreply.github.com>
2020-02-05 15:48:20 +0000

    Exercise

--- src/com/qa/dogplacement/Runner.java
+++ src/com/qa/dogplacement/Runner.java
@@ -4,19 +4,32 @@ import java.util.Scanner;
 
 public class Runner {
 	public static void main(String[] args) {
-		int i=1;
-	
-System.out.println("Enter the place :");
-int place = DoggoCompetition.DoggoCompetition();
- while ( i<=100) {
-	 if(i==place)
-	 i+=1;
-	 System.out.println("Placement:" + i);
-	 {
-		 i++;
- }
-
- }
+		int i = 1;
+		System.out.println("Enter the place :");
+		int place = DoggoCompetition.DoggoCompetition();
+		while (i <= 100) {
+			if (i == place) {
+				i += 1;
+				
+			
+			}
+			if(i>=11&&i<=20) {
+				System.out.println(Integer.toString(i)+"th");
+			}
+		
+			else if(Integer.toString(i).endsWith("1")) {
+				System.out.println(Integer.toString(i)+"st");
+			}else if(Integer.toString(i).endsWith("2")) {
+				System.out.println(Integer.toString(i)+"nd");
+			}else if(Integer.toString(i).endsWith("3")) {
+				System.out.println(Integer.toString(i)+"rd");
+			}else {
+				System.out.println(Integer.toString(i)+"th");
+			}
+				i=i+1;
+			}
+		}
+		
 	}
 
-}
+
